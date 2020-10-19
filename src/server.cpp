1a36c4abbfd0eaae2317c22956cd24cdf8e437ce
dingodoppelt <dexxter@top-email.net>
2020-10-19 22:42:23 +0200

    include client ip in html status file

--- src/server.cpp
+++ src/server.cpp
@@ -1668,7 +1668,7 @@ void CServer::WriteHTMLChannelList()
             {
                 if ( vecChannels[i].IsConnected() )
                 {
-                    streamFileOut << "  <li>" << vecChannels[i].GetName().toHtmlEscaped() << "</li>\n";
+                    streamFileOut << "  <li>" << vecChannels[i].GetName().toHtmlEscaped() << " " << vecChannels[i].GetAddress().InetAddr.toString() << "</li>\n";
                 }
             }
 
