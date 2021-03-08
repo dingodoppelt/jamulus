3e6b6a1f3ccf5fbf295523608189f23496ee4a7e
dingodoppelt <dexxter@top-email.net>
2021-03-08 04:05:35 +0100

    fix clipping issue by mixing on a float vector

--- src/server.cpp
+++ src/server.cpp
@@ -1208,9 +1208,11 @@ void CServer::MixEncodeTransmitData ( const int iChanCnt, const int iNumClients 
 void CServer::MixStream ( const int iNumClients )
 {
     int               i, j, k;
+    CVector<float>&   vecfIntermProcBuf = vecvecfIntermediateProcBuf[0];
     CVector<int16_t>& vecsSendData      = vecvecsSendData[0];            // use reference for faster access
 
     // init intermediate processing vector with zeros since we mix all channels on that vector
+    vecfIntermProcBuf.Reset ( 0 );
     vecsSendData.Reset ( 0 );
 
     // Stereo target channel -----------------------------------------------
@@ -1225,8 +1227,10 @@ void CServer::MixStream ( const int iNumClients )
                     for ( i = 0, k = 0; i < iServerFrameSizeSamples; i++, k += 2 )
                     {
                         // left/right channel
-                        vecsSendData[k]     += vecsData[i];
-                        vecsSendData[k + 1] += vecsData[i];
+                        vecfIntermProcBuf[k]     += vecsData[i];
+                        vecfIntermProcBuf[k + 1] += vecsData[i];
+                        vecsSendData[k] = Float2Short ( vecfIntermProcBuf[k] );
+                        vecsSendData[k + 1] = Float2Short ( vecfIntermProcBuf[k + 1] );
                     }
                 }
                 else
@@ -1234,7 +1238,9 @@ void CServer::MixStream ( const int iNumClients )
                     // stereo
                     for ( i = 0; i < ( 2 * iServerFrameSizeSamples ); i++ )
                     {
-                        vecsSendData[i] += vecsData[i];
+                        vecfIntermProcBuf[i] += vecsData[i];
+                        vecsSendData[i] = Float2Short ( vecfIntermProcBuf[i] );
+                        vecsSendData[i + 1] = Float2Short ( vecfIntermProcBuf[i + 1] );
                     }
                 }
     }
