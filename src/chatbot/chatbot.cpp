c8d6e8f3bcb7ec1c8b53d16b79218272117b408e
dingodoppelt <dexxter@top-email.net>
2021-04-06 11:31:16 +0200

    fix filename

--- src/chatbot/chatbot.cpp
+++ src/chatbot/chatbot.cpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <QByteArray> 
 
 using namespace chatbot;
 
@@ -13,13 +14,14 @@ CChatBot::CChatBot()
 
 void CChatBot::Init( const quint16 iPortNumber )
 {
-    this->chatFile = QString("/tmp/JamChat-%1").arg(iPortNumber).toUtf8().constData();
-    mkfifo(this->chatFile, 0777);
+    QByteArray str = QString("/tmp/JamChat-%1").arg(iPortNumber).toUtf8();
+    chatFile = strdup(str.constData());
+    mkfifo(chatFile, 0777);
 }
 
 void CChatBot::OnIntChatMessReceived( const QString strChatMess )
 {
-    int fd = open(this->chatFile, O_WRONLY | O_NONBLOCK);
+    int fd = open(chatFile, O_WRONLY | O_NONBLOCK);
     write(fd, strChatMess.toLatin1().data(), strlen(strChatMess.toLatin1().data()));
     //close(fd);
 }
